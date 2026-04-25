74e7d119f2215c68f19678c833d44eb97ade37f6
natefavorite-png <natefavorite@gmail.com>
2026-04-25 21:33:21 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,26 @@
+Starting Container
+SyntaxError: invalid character '“' (U+201C)
+SyntaxError: invalid character '“' (U+201C)
+    CONFIG_FILE = “config.json”
+SyntaxError: invalid character '“' (U+201C)
+  File "/app/bot.py", line 16
+    CONFIG_FILE = “config.json”
+                  ^
+SyntaxError: invalid character '“' (U+201C)
+  File "/app/bot.py", line 16
+    CONFIG_FILE = “config.json”
+                  ^
+    CONFIG_FILE = “config.json”
+                  ^
+    CONFIG_FILE = “config.json”
+  File "/app/bot.py", line 16
+    CONFIG_FILE = “config.json”
+                  ^
+SyntaxError: invalid character '“' (U+201C)
+  File "/app/bot.py", line 16
+    CONFIG_FILE = “config.json”
+                  ^
+  File "/app/bot.py", line 16
+    CONFIG_FILE = “config.json”
+                  ^
+SyntaxError: invalid character '“' (U+201C)
